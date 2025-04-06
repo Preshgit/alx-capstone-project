6a4a86c0312a1fffc451de6e1de4f796e7e1db41
preshgit <preciousdesk10@gmail.com>
2025-04-06 16:56:35 +0100

    feature: install axios

---
+++ src/app/axios.config.ts
@@ -0,0 +1,8 @@
+// import { axios } from "axios";
+
+// export const customAxios = axios.create({
+//   baseUrl: process.env.NEXT_BASE_URL || "https://nigerian-art.vercel.app", // backend url
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
